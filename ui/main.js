6e1f1462f2c2d7a38a720a33e95e7424a71b7616
shefi7878 <shefi7878@gmail.com>
2016-10-05 10:32:37 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('img');
+var marginleft =0;
+function moveRight () {
+    marginleft = marginleft + 1;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function () {
     img.style. marginleft = '100px';
 };
